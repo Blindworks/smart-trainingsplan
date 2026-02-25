f2e4a9027653585310edc5af6480ce93845db08c
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2026-02-25 18:45:19 +0100

    Host fixed for TEST env

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: `${window.location.protocol}//${window.location.hostname}:8080/api`
+  apiUrl: `${window.location.protocol}//${window.location.hostname}:${window.location.hostname === 'localhost' ? '8080' : '8081'}/api`
 };
